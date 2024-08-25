c10e958ef08cfde619f4f4d166aa37ee7dc11fc0
Sencyy <sencyy@protonmail.com>
2024-08-25 19:09:56 -0300

    add main loop

--- posturereminder.c
+++ posturereminder.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <libnotify/notify.h>
 
 int main(void) {
-    const char NOTIFICATION_TITLE[] = "Posture Reminder";
-    const char NOTIFICATION_MESSAGE[] = "This is a reminder to fix your posture!";
+      const char NOTIFICATION_TITLE[] = "Posture Reminder";
+      const char NOTIFICATION_MESSAGE[] = "This is a reminder to fix your posture!";
+      const unsigned int TIME_BETWEEN_NOTIFICATIONS = 1800; // In seconds
 
-    notify_init(NOTIFICATION_TITLE);
-    NotifyNotification * Notification = notify_notification_new(NOTIFICATION_TITLE, NOTIFICATION_MESSAGE, "dialog-information");
+      printf("Notifications will be sent each %d minutes.\n", TIME_BETWEEN_NOTIFICATIONS / 60);
+  
+      while(true){
+           
+        sleep(TIME_BETWEEN_NOTIFICATIONS);
 
-    notify_notification_show(Notification, NULL);
+        notify_init(NOTIFICATION_TITLE);
+        NotifyNotification * Notification = notify_notification_new(NOTIFICATION_TITLE, NOTIFICATION_MESSAGE, "dialog-information");
 
-    g_object_unref(G_OBJECT(Notification));
-    notify_uninit();
+        notify_notification_show(Notification, NULL);
 
-    return 0;
+        g_object_unref(G_OBJECT(Notification));
+        notify_uninit();
+       
+      }
+      return 0;
 
 }
